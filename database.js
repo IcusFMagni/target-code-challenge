25b06e0d920270a5cc76c1c7d72c2b75f84ace2e
IcusFMagni <danika.ragnhild@gmail.com>
2018-04-13 11:09:42 -0500

    update dabase.js and delete unused styling

--- database.js
+++ database.js
@@ -10,18 +10,17 @@ db.createCollection('products');
 /* show collections */
 show collections;
 
-/* add movies to collection */
-db.item.insert({
-    title: 'Star Wars: A New Hope',
-    urlOfPic: '../images/Star_Wars_A_New_Hope.jpeg',
-    description: 'The Imperial Forces, under orders from cruel Darth Vader, hold Princess Leia hostage in their efforts to quell the rebellion against the Galactic Empire. Luke Skywalker and Han Solo, captain of the Millennium Falcon, work together with the companionable droid duo R2-D2 and C-3PO to rescue the beautiful princess, help the Rebel Alliance and restore freedom and justice to the Galaxy.',
-    rating: 3
+/* add items to collection  */
+db.products.insert({
+    id: 13860428, 
+    cost: 19.99,
+    currency: "USD"
 });
 
 
 
-/* get movies to collection */
+/* get items to collection */
 db.products.find().pretty();
 
-/* get a movie by title collection */
-db.products.find({title: 'Star Wars: A New Hope'}).pretty();
+/* get an item by id collection */
+db.products.find({id: 13860428}).pretty();
