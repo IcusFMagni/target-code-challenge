fc7d13c89af817e9f58ec784bce253051daaa6b0
IcusFMagni <danika.ragnhild@gmail.com>
2018-04-13 13:43:29 -0500

    removed front end.  API still works just fine!

--- server/public/scripts/controllers/search.controller.js
+++
@@ -1,7 +0,0 @@
-app.controller('SearchController', ['ItemService', function (ItemService) {
-    var self = this;
-    self.searchTerm = ItemService.searchTerm;
-    self.item = ItemService.item;
-    self.searchItem = ItemService.searchItem;
-    }
-]);
