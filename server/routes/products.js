baa5f625c9bab9cfa1bc22e75f8b8d8227281a2d
IcusFMagni <danika.ragnhild@gmail.com>
2018-04-13 13:54:41 -0500

    add comments to product.js

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,12 +4,15 @@ var Item = require('../models/items.schema')
 var request = require('request')
 
 router.get('/:id', function (req, res) {
+    //Finds the product on Redsky to retrieve it's name.
     request({
         uri: "https://redsky.target.com/v2/pdp/tcin/" + req.params.id
     }, function (err, response, body) {
         if (!err && response.statusCode == 200) {
             let item = {
                 title: JSON.parse(body).product.item.product_description.title}
+
+                //Finds the price on seperate Database
             Item.findOne({ id: req.params.id }, function (error, data) {
                 if (error) {
                     res.sendStatus(500)
